9e4c794726ccebab49cc10b976cd1d607026fec2
Rony Stico <ronystico@gmail.com>
2021-11-09 17:38:29 -0400

    hemos, hemos. he hemos, hemos migrado a .NET 6

--- Escuela/Program.cs
+++ Escuela/Program.cs
@@ -1,26 +1,89 @@
-using Microsoft.AspNetCore.Hosting;
+using Escuela.Data;
+using Escuela.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Escuela
+var builder = WebApplication.CreateBuilder(args);
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+}).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddAuthorization();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = false;
+    options.Password.RequireUppercase = false;
+});
+
+builder.Services.AddControllers(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser()
+                        .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Inicio}/{action=Inicio}/{id?}");
+        endpoints.MapRazorPages();
+        endpoints.MapAreaControllerRoute(
+        name: "Administracion",
+        areaName: "Administracion",
+        pattern: "{controller=Usuario}/{action=Inicio}/{id?}/{id2?}/{referencia?}/{rol?}"
+        );
+        endpoints.MapAreaControllerRoute(
+        name: "Profesor",
+        areaName: "Profesor",
+        pattern: "{controller=Calificacion}/{action=Inicio}/{id?}/{id2?}"
+        );
+        endpoints.MapAreaControllerRoute(
+        name: "Estudiante",
+        areaName: "Estudiante",
+        pattern: "{controller=Calificaciones}/{action=Inicio}/{id?}/{id2?}"
+        );
+    });
+
+app.MapRazorPages();
+
+app.Run();
